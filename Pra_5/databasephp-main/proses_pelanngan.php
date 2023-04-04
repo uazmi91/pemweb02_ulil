9a6fe4a66c76fb736c1452d76b19d69054515380
Ulil Azmi <115434148+uazmi91@users.noreply.github.com>
2023-04-04 11:50:43 +0700

    Add files via upload

---
+++ Pra_5/databasephp-main/proses_pelanngan.php
@@ -0,0 +1,38 @@
+<?php 
+require_once 'dbkoneksi.php';
+?>
+<?php 
+   $_kode = $_POST['kode'];
+   $_nama = $_POST['nama'];
+   $_jk = $_POST['jk'];
+   $_tmp_lahir = $_POST['tmp_lahir'];
+   $_minstok = $_POST['tgl_lahir'];
+   $_jenis = $_POST['kartu_id'];
+   $_jenis = $_POST['email'];
+   $_proses = $_POST['proses'];
+
+   // array data
+   $ar_data[]=$_kode; // ? 1
+   $ar_data[]=$_nama; // ? 2
+   $ar_data[]=$_jk;// 3
+   $ar_data[]=$_tmp_lahir;
+   $ar_data[]=$_tgl_lahir;
+   $ar_data[]=$_email;
+   $ar_data[]=$_kartu_id; // ? 7
+
+   if($_proses == "Simpan"){
+    // data baru
+    $sql = "INSERT INTO produk (kode,nama,harga_beli,harga_jual,stok,
+    min_stok,jenis_produk_id) VALUES (?,?,?,?,?,?,?)";
+   }else if($_proses == "Update"){
+    $ar_data[]=$_POST['idedit'];// ? 8
+    $sql = "UPDATE produk SET kode=?,nama=?,harga_beli=?,harga_jual=?,
+    stok=?,min_stok=?,jenis_produk_id=? WHERE id=?";
+   }
+   if(isset($sql)){
+    $st = $dbh->prepare($sql);
+    $st->execute($ar_data);
+   }
+
+   header('location:list_produk.php');
+?>
